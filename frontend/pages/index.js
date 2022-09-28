aaa998c52eb0456d55bb59eeecec2bdd6e50d8d4
Afif Fatur Rahman <afiffatur525@gmail.com>
2022-09-28 13:32:51 +0700

    added post method and submit on web

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -10,27 +10,70 @@ export default function Home() {
     getData()
   }, []);
 
+  console.log(data?.data)
   const getData = async () => {
     try {
       const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL)
       const data = await res.json();
       setData(data)
-    } catch (error) {
+    } 
+    catch (error) {
       setError(error);
     }
   }
 
-  if (error) {
-    return <div>Failed to load {error.toString()}</div>
-  }
-
-  if (!data) {
-    return <div>Loading...</div>
-  }
-
   return (
     <div>
-      <p>data: {data.message}</p>
+      {error && <div>Failed to load (error.toString())</div>}
+      {
+        !data ? <div>Loading...</div>
+          : (
+            (data?.data ?? []).length === 0 && <p>data kosong</p>
+          )
+      }
+      <Input onSuccess={getData}/>
+      {data?.data && data?.data?.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+    </div>
+  
+  )
+}
+
+
+function Input({onSuccess}) {
+  const [data, setData] =  useState(null);
+  const [error, setError] =  useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const body = {
+      text: formData.get("data")
+    }
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`,{
+        method :'POST',
+        body : JSON.stringify(body)
+      });
+      const data = await res.json();
+      setData(data.message);
+      onSuccess();
+    } 
+    catch (error) {
+      setError(error);
+    }
+  }
+  return (
+    <div>
+      {error && <p>error: {error.toString()}</p>}
+      {data && <p>success: {data}</p>}
+      <form onSubmit={handleSubmit}>
+        <input name="data" type="text" />
+        <button>submit</button>
+      </form>
     </div>
   )
 }
+
